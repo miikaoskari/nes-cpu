052d78e9f26c1ea2629ce3898eb32fe633a52bca
miikaoskari <miika.karjalainen13@gmail.com>
2024-05-18 18:16:58 +0300

    implement opcode decoding logic in Instruction_Register

--- src/instruction_register.vhd
+++ src/instruction_register.vhd
@@ -1,11 +1,22 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
 entity Instruction_Register is
     Port (
         opcode : in std_logic_vector(7 downto 0);
-        control : out std_logic_vector(15 downto 0);
+        control : out std_logic_vector(15 downto 0)
     );
 end entity Instruction_Register;
 
 architecture Behavioral of Instruction_Register is
 begin
-
+    process(opcode)
+    begin
+        case opcode is
+            when "00000001" =>
+                control <= "0000000000000001";
+            when others => -- TODO: handle other cases
+                control <= (others => '0');
+        end case;
+    end process;
 end Behavioral;
