da736e5e0742f2a2ff4ab038885d5d0d5709c624
miikaoskari <miika.karjalainen13@gmail.com>
2024-05-19 20:54:40 +0300

    chore: Update Memory entity declaration

--- src/memory.vhdl
+++ src/memory.vhdl
@@ -9,7 +9,7 @@ entity Memory is
        write_enable : in std_logic;
        data_out : out std_logic_vector(7 downto 0)
     );
-end Memory;
+end entity Memory;
 
 architecture Behavioral of Memory is
     type memory_array is array (0 to 65535) of std_logic_vector(7 downto 0);
