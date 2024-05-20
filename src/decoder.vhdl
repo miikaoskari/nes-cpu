e08f722ce841e999725efa37efa39eb3192c5939
miikaoskari <miika.karjalainen13@gmail.com>
2024-05-21 00:39:07 +0300

    refactor: Fix control_signals width in Decoder entity

--- src/decoder.vhdl
+++ src/decoder.vhdl
@@ -6,7 +6,7 @@ entity Decoder is
         clk : in std_logic;
         reset : in std_logic;
         opcode : in std_logic_vector(7 downto 0);
-        control : out std_logic_vector(7 downto 0)
+        control : out std_logic_vector(15 downto 0)
     );
 end entity Decoder;
 
@@ -16,7 +16,7 @@ begin
     begin
         case opcode is
             when x"69" | x"65" | x"75" | x"6D" | x"7D" | x"79" | x"61" | x"71" -- adc
-
+                control
                 
             when others => -- TODO: handle other cases
                 control <= (others => '0');
